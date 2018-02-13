a268d18a91d5c9d7c07436b0c745fe16cbc8cbc6
Mahapatra <sm98006@nsroot.net>
2018-02-13 19:16:26 +0530

    Angular Dynamic Form implemented

--- src/app/modules/sign-on/sign-on.component.ts
+++ src/app/modules/sign-on/sign-on.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+
+import { FieldConfig } from '../../shared/modules/dynamic-form/models/field-config.interface';
+import { DynamicFormComponent } from '../../shared/modules/dynamic-form/containers/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-sign-on',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-on.component.css']
 })
 export class SignOnComponent implements OnInit {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
 
+  config: FieldConfig[] = [
+    {
+      type: 'input',
+      label: 'Full name',
+      name: 'name',
+      placeholder: 'Enter your name',
+      validation: [Validators.required, Validators.minLength(4)]
+    },
+    {
+      type: 'select',
+      label: 'Favourite Food',
+      name: 'food',
+      options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+      placeholder: 'Select an option',
+      validation: [Validators.required]
+    },
+    {
+      label: 'Submit',
+      name: 'submit',
+      type: 'button'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  
+
+  submit(value: {[name: string]: any}) {
+    console.log(value);
   }
 
 }
--- src/app/modules/sign-on/sign-on.module.ts
+++ src/app/modules/sign-on/sign-on.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
-
+import { DynamicFormModule } from '../../shared/modules/dynamic-form/dynamic-form.module';
 import { SignOnComponent } from './sign-on.component';
 import { SignOnRoutingModule } from './sign-on-routing.module';
 
@@ -11,7 +11,8 @@ import { SignOnRoutingModule } from './sign-on-routing.module';
     CommonModule,
     RouterModule,
 
-    SignOnRoutingModule
+    SignOnRoutingModule,
+    DynamicFormModule
   ],
   declarations: [SignOnComponent],
 })
