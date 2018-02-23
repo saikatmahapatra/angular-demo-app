f2d7a3b4e41f9b405062c1a132a25ab34c256319
Mahapatra <sm98006@nsroot.net>
2018-02-23 14:47:38 +0530

    using service in aservice

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { LoggerService } from './logger.service';
 @Injectable()
 export class CommonService {
 
-  constructor() { }
+  constructor(private _lg: LoggerService) { }
 
   someMethod() {
+    this._lg.log("Using LoggerService in CommonService");
     return 'Hey! Im CommonService :)';
   }
 
