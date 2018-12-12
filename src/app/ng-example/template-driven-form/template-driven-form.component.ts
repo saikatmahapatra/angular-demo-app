b3f3c943b94694e6b38ed3532a2cc6a95651fc54
Saikat <mahapatra.saikat@gmail.com>
2018-12-12 19:10:57 +0530

    updated

--- src/app/ng-example/template-driven-form/template-driven-form.component.ts
+++ src/app/ng-example/template-driven-form/template-driven-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validator } from '@angular/forms';
-import { User } from '../../shared/models/user'; // Import the user class for form example
+import { User } from '../../shared/models/index'; // Import the user class for form example
 import { ContentService, LoggerService, ValidationService } from '../../shared/services/index';
 @Component({
   selector: 'app-template-driven-form',
