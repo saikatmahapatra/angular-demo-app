cf2717cb3bd0c18a28bd4a476a3fbfb9f8580c45
Saikat <mahapatra.saikat@gmail.com>
2019-01-24 12:00:33 +0530

    ui change

--- src/app/ng-example/observable-in-angular/employee.component.ts
+++ src/app/ng-example/observable-in-angular/employee.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +13,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
