558ee0062007bac0e8a8a18b168fddfe2969dffc
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-02-17 20:52:07 +0530

    architectural change

--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { PaymentRoutingModule } from './payment-routing.module';
 import { PaymentLayoutComponent } from './payment-layout.component';
@@ -9,6 +11,7 @@ import { DynamicComponentLoaderService } from '../shared/common-services/dynamic
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     PaymentRoutingModule,
     SharedModule
   ],
