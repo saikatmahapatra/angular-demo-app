21b2c410355047ba95db9d9c45a863cfea057ada
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2020-10-29 18:00:21 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,6 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})]
+  imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule { }
