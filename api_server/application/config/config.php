2b3f0574173755b76c0fac0e3826ceab1cf6bef0
= <=>
2021-10-26 18:51:38 +0530

    renamed

--- api_server/application/config/config.php
+++ api_server/application/config/config.php
@@ -17,7 +17,7 @@ date_default_timezone_set('Asia/Kolkata');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/angular-demo-app/app_backend/';
+$config['base_url'] = 'http://localhost/angular-demo-app/api_server/';
 
 /*
 |--------------------------------------------------------------------------
