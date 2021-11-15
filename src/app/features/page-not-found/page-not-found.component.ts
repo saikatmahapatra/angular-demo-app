e6861d3975d6eea6411766a6e8f934cf7e5a9de4
= <=>
2021-11-15 22:58:10 +0530

    404 redirect issue fixed

--- src/app/features/page-not-found/page-not-found.component.ts
+++ src/app/features/page-not-found/page-not-found.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
   }
 
 }
