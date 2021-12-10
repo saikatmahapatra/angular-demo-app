9441bd57d31769677475240a59be0a97c565b1c8
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-12-10 17:16:12 +0530

    mat modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { environment } from '../environments/environment';
-import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 import { AppComponent } from './app.component';
@@ -11,6 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,6 +29,12 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     RouterModule,
     MatDialogModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
