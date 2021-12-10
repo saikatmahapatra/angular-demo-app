b06317fd52b614741455303a0f80f099e4aa0279
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-12-10 17:16:23 +0530

    modified

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    SharedModule,
+    FlexLayoutModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginModule { }
