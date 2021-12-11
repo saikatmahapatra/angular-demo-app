fe9fc7fec33e529081397db1c4b4b43421ec7207
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-12-11 20:23:51 +0530

    menu modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
     ReactiveFormsModule,
     CoreModule,
     SharedModule,
+    MatButtonModule,
     // Routing modules must be at the last and AppRouting Module must be the last one.
     AppRoutingModule
   ],
