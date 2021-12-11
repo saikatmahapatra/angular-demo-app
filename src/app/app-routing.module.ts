e371618bea5406020c02518abb043de28a4078c6
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-12-11 10:53:05 +0530

    dashboard module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PageNotFoundComponent } from './features/page-not-found/page-not-found.
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'pages', loadChildren: () => import('./features/page/page.module').then(m => m.PageModule) },
   { path: 'angular-example', loadChildren: () => import('./features/ng-example/ng-example.module').then(m => m.NgExampleModule) },
   { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
