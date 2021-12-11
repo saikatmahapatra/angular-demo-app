436fc8ebefe7b4a4cc278a8f31d63dacea6a2c70
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-12-11 21:16:52 +0530

    guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 import { DefaultLayoutComponent } from './shared/components/layouts/default-layout/default-layout.component';
 import { UnauthenticatedLayoutComponent } from './shared/components/layouts/unauthenticated-layout/unauthenticated-layout.component';
@@ -13,7 +14,7 @@ const routes: Routes = [
     path: '',
     component: DefaultLayoutComponent,
     children: [
-      { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+      { path: 'home', canActivate: [AuthGuard], loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
     ]
   },
   {
@@ -27,10 +28,10 @@ const routes: Routes = [
     path: '',
     component: AuthenticatedLayoutComponent,
     children: [
-      { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
-      { path: 'pages', loadChildren: () => import('./features/page/page.module').then(m => m.PageModule) },
-      { path: 'angular-example', loadChildren: () => import('./features/ng-example/ng-example.module').then(m => m.NgExampleModule) },
-      { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }
+      { path: 'dashboard', canActivate: [AuthGuard], loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
+      { path: 'pages', canActivate: [AuthGuard], loadChildren: () => import('./features/page/page.module').then(m => m.PageModule) },
+      { path: 'angular-example', canActivate: [AuthGuard], loadChildren: () => import('./features/ng-example/ng-example.module').then(m => m.NgExampleModule) },
+      { path: 'user', canActivate: [AuthGuard], loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }
     ]
   },
   { path: '**', component: PageNotFoundComponent } // wildcard will be at always last
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router, private authService: AuthService) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    let routeMessage: string = '';
+    const isTokenExpired = this.authService.isTokenExpired();
+
+    if (!isTokenExpired) {
+      const isLoggedIn = this.authService.isLoggedIn();
+
+      if (isLoggedIn) {
+        return true;
+      } else {
+        routeMessage = "You must login to continue.";
+      }
+    } else {
+      routeMessage = "Your session has expired."
+    }
+
+    //if (routeMessage) this.alertService.info(routeMessage, { keepAfterRouteChange: false });
+    console.log(routeMessage);
+
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
   
 }
