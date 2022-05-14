7d4838755f1b06c42e79f587789833cf2bb8deb0
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-14 16:58:10 +0530

    headers set

--- server/api_server/index.php
+++ server/api_server/index.php
@@ -1,4 +1,7 @@
 <?php
+Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
 /**
  * CodeIgniter
  *
