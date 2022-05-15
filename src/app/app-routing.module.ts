78ef6694a5309d06dc0b255d8d3102776d772488
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-15 11:55:28 +0530

    path modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
-import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DefaultLayoutComponent } from './shared/components/layouts/default-layout/default-layout.component';
 import { UnauthenticatedLayoutComponent } from './shared/components/layouts/unauthenticated-layout/unauthenticated-layout.component';
 import { AuthenticatedLayoutComponent } from './shared/components/layouts/authenticated-layout/authenticated-layout.component';
@@ -14,17 +14,17 @@ const routes: Routes = [
     path: '',
     component: UnauthenticatedLayoutComponent,
     children: [
-      { path: 'auth', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }
+      { path: 'auth', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
     ]
   },
   {
     path: '',
     component: AuthenticatedLayoutComponent,
     children: [
-      { path: 'dashboard', canActivate: [AuthGuard], loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
-      { path: 'pages', canActivate: [AuthGuard], loadChildren: () => import('./features/page/page.module').then(m => m.PageModule) },
-      { path: 'angular-example', canActivate: [AuthGuard], loadChildren: () => import('./features/ng-example/ng-example.module').then(m => m.NgExampleModule) },
-      { path: 'user', canActivate: [AuthGuard], loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }
+      { path: 'dashboard', canActivate: [AuthGuard], loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+      { path: 'pages', canActivate: [AuthGuard], loadChildren: () => import('./page/page.module').then(m => m.PageModule) },
+      { path: 'angular-example', canActivate: [AuthGuard], loadChildren: () => import('./ng-example/ng-example.module').then(m => m.NgExampleModule) },
+      { path: 'user', canActivate: [AuthGuard], loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
     ]
   },
   { path: '**', component: PageNotFoundComponent } // wildcard will be at always last
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
