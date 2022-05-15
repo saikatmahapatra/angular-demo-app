031ae8f4f210df3e94b81c563800c0e813b1c234
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-15 11:57:20 +0530

    path fixed

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
