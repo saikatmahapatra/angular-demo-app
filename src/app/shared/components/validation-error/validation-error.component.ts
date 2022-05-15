db55bbd4e357b5cb88de700e68e1cfe83b75a5b4
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-15 23:29:55 +0530

    validateAllFormFields once form is submitted

--- src/app/shared/components/validation-error/validation-error.component.ts
+++ src/app/shared/components/validation-error/validation-error.component.ts
@@ -23,6 +23,16 @@ export class ValidationErrorComponent implements OnInit {
 
 
 
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
   
   //get err message
   get errorMessage() {
