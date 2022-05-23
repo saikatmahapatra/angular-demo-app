7d3001b73f12f52027be9451bade3acdae681297
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-23 16:31:17 +0530

    header moved

--- server/ci-api-server/application/controllers/api/V1.php
+++ server/ci-api-server/application/controllers/api/V1.php
@@ -1,9 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age:86400');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
--- server/ci-api-server/index.php
+++ server/ci-api-server/index.php
@@ -1,4 +1,9 @@
 <?php
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Max-Age:86400');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With');
 /**
  * CodeIgniter
  *
