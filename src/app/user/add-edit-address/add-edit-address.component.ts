d117ccc5ee07fb3d086d9aec7bf933d19e7120d8
Saikat <mahapatra.saikat@gmail.com>
2022-08-01 10:09:16 +0530

    Address add form modified

--- src/app/user/add-edit-address/add-edit-address.component.ts
+++ src/app/user/add-edit-address/add-edit-address.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { ApiService } from 'src/app/@core/services/api.service';
+import { FormValidationService } from 'src/app/@core/services/form-validation.service';
+
 
 @Component({
   selector: 'app-add-edit-address',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, 
+    private validator: FormValidationService,
+    private apiSvc: ApiService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    // get action name & id for PUT call
+  }
+
+  myForm = this.fb.group({
+    id: [null],
+    action: ['add'],
+    addressLine1: ['', [Validators.required]],
+    addressLine2: ['', [Validators.required]],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    zip: ['', Validators.required]
+  });
+
+  onSubmit() {
+    console.log('onSubmit===', this.myForm);
+    if (this.myForm.valid) {
+      console.log('form submitted', this.myForm.value);
+    } else {
+      this.validator.validateAllFormFields(this.myForm);
+    }
   }
 
 }
