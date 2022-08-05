ecf59ecd6107856da82e277050dc573ad81e3aeb
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-08-05 10:41:03 +0530

    global spinner implemented

--- src/app/@core/interceptors/auth.interceptor.ts
+++ src/app/@core/interceptors/auth.interceptor.ts
@@ -5,25 +5,31 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { SpinnerService } from '../services/spinner.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authSvc: AuthService) {}
+  constructor(private authSvc: AuthService, private spinnerSvc: SpinnerService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authToken = this.authSvc.getToken();
+    this.spinnerSvc.show();
     if(authToken) {
       const clonedReq = request.clone({
         setHeaders: {
           Authorization: authToken
         }
       });
-      return next.handle(clonedReq);
+      return next.handle(clonedReq).pipe(
+        finalize(() => this.spinnerSvc.hide()),
+      );
     } else {
-      return next.handle(request);
+      return next.handle(request).pipe(
+        finalize(() => this.spinnerSvc.hide()),
+      );
     }
   }
 }
