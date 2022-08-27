c167fb4a72568f2d6b2be5fb06bd76ad25a33ece
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-08-27 18:05:39 +0530

    observable

--- src/app/ng-example/observable-in-angular/employee.component.ts
+++ src/app/ng-example/observable-in-angular/employee.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-
-
-
-
-
-
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html'
@@ -17,11 +11,30 @@ export class EmployeeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
+    this.simpleObservable();
   }
   
   ctaClick(event: any) {
     alert('you have clicked');
+  }
+
+  simpleObservable() {
+    let O = new Observable(observer => {
+      setTimeout(function(){
+        observer.next(1);
+      }, 2000);
+    });
+    console.log('Observable invoked');
+    O.subscribe();
+    console.log("Observable", O);
+  }
+
+  subscribe() {
+    console.log('subscribe btn clicked');
+  }
+
+  unsubscribe() {
+    console.log('unsubscribe btn clicked');
   }
 
 }
