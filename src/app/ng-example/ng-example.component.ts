3026f958726bfe2280d1a5670d276774619e3de9
Saikat <mahapatra.saikat@gmail.com>
2022-09-06 17:30:44 +0530

    link modified

--- src/app/ng-example/ng-example.component.ts
+++ src/app/ng-example/ng-example.component.ts
@@ -9,6 +9,23 @@ export class NgExampleComponent implements OnInit {
   title = 'Angular4';
   subtitle = 'Fundamental of Angular 2';
 
+  menuLinks = [
+    { link: '/', text: 'App Home' },
+    { link: '/example/template-basic', text: 'Template Basic' },
+    { link: '/example/types-of-angular-directive', text: 'Directive' },
+    { link: '/example/pipes', text: 'Pipes' },
+    { link: '/example/user-input-binding', text: 'Data Binding' },
+    { link: '/example/template-driven-form', text: 'Template Driven Form' },
+    { link: '/example/reactive-form', text: 'Reactive Form' },
+    { link: '/example/angular-services', text: 'Use of Service' },
+    { link: '/example/parent-comp', text: 'Parent-Child Component' },
+    { link: '/example/employee', text: 'Observable (RxJS Operators)' },
+    { link: '/example/tranfer-fund', text: 'Routing Basic' },
+    { link: '/example/content-projection', text: 'Content Projection' },
+    { link: '/example/ag-grid', text: 'AG Grid Demo' },
+    { link: '/example/translate', text: 'i18n Translation' },
+  ];
+
   constructor(private commonSvc: CommonService) { }
 
   /**
