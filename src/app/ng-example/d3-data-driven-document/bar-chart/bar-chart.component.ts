20fcd37c815d709cd05266dc2566de8a66d16309
Saikat <mahapatra.saikat@gmail.com>
2022-09-07 15:44:11 +0530

    bar chart modified

--- src/app/ng-example/d3-data-driven-document/bar-chart/bar-chart.component.ts
+++ src/app/ng-example/d3-data-driven-document/bar-chart/bar-chart.component.ts
@@ -8,14 +8,14 @@ import * as d3 from 'd3';
 })
 export class BarChartComponent implements OnInit {
   private data = [
-    { "Framework": "Vue", "Stars": "166443", "Released": "2014" },
-    { "Framework": "React", "Stars": "150793", "Released": "2013" },
-    { "Framework": "Angular", "Stars": "62342", "Released": "2016" },
-    { "Framework": "Backbone", "Stars": "27647", "Released": "2010" },
-    { "Framework": "Ember", "Stars": "21471", "Released": "2011" },
+    { "Product": "iPhone 6s", "SoldUnit": "236", "Released": "2010" },
+    { "Product": "iPhone 12 mini", "SoldUnit": "150", "Released": "2022" },
+    { "Product": "iPhone 13", "SoldUnit": "390", "Released": "2021" },
+    { "Product": "iPhone 12 Pro", "SoldUnit": "244", "Released": "2021" },
+    { "Product": "iPhone 11", "SoldUnit": "122", "Released": "2019" },
   ];
   private svg: any;
-  private margin = 50;
+  private margin = 100;
   private width = 750 - (this.margin * 2);
   private height = 400 - (this.margin * 2);
   constructor() { }
@@ -37,20 +37,21 @@ export class BarChartComponent implements OnInit {
     // Create the X-axis band scale
     const x = d3.scaleBand()
       .range([0, this.width])
-      .domain(data.map(d => d.Framework))
+      .domain(data.map(d => d.Product))
       .padding(0.2);
 
     // Draw the X-axis on the DOM
     this.svg.append("g")
-      .attr("transform", "translate(0," + this.height + ")")
+      .attr("transform", "translate(0," + (this.height) + ")")
       .call(d3.axisBottom(x))
       .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+      .style("text-anchor", "end")
+      .style("font-size", "14px");
 
     // Create the Y-axis band scale
     const y = d3.scaleLinear()
-      .domain([0, 200000])
+      .domain([0, 500])
       .range([this.height, 0]);
 
     // Draw the Y-axis on the DOM
@@ -62,10 +63,10 @@ export class BarChartComponent implements OnInit {
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d: { Framework: string; }) => x(d.Framework))
-      .attr("y", (d: { Stars: d3.NumberValue; }) => y(d.Stars))
+      .attr("x", (d: { Product: string; }) => x(d.Product))
+      .attr("y", (d: { SoldUnit: d3.NumberValue; }) => y(d.SoldUnit))
       .attr("width", x.bandwidth())
-      .attr("height", (d: { Stars: d3.NumberValue; }) => this.height - y(d.Stars))
+      .attr("height", (d: { SoldUnit: d3.NumberValue; }) => this.height - y(d.SoldUnit))
       .attr("fill", "#d04a35");
   }
 
