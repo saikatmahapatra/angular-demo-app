9cf8bf2103371b379eeff3163c5bcb7c767bdc80
Saikat <mahapatra.saikat@gmail.com>
2022-09-12 10:35:05 +0530

    event printed

--- src/app/@shared/components/file-upload/file-upload.component.ts
+++ src/app/@shared/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { event } from 'jquery';
 
 @Component({
   selector: 'app-file-upload',
@@ -10,6 +11,9 @@ export class FileUploadComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onFileSelected($event: any) {
+    console.log("File Upload", event);
   }
 
 }
