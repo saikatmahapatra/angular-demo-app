5d4e1e56a7b47a2aeb6d5a497eecbc4581e7eb34
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-09-17 19:40:07 +0530

    time diff added

--- src/app/@core/services/common.service.ts
+++ src/app/@core/services/common.service.ts
@@ -39,7 +39,7 @@ export class CommonService {
       console.error(msg);
     }
   }
-  
+
   scrollToTop() {
     window.document.body.scrollTop = 0;
     window.document.documentElement.scrollTop = 0;
@@ -87,5 +87,41 @@ export class CommonService {
   // }
   setCookieValue(key: string, value: any) {
     document.cookie = key + '=' + value;
+  }
+
+  timeDifference(phpTimeStamp: string) {
+    let current = new Date().getTime();
+    let previous = new Date(Date.parse(phpTimeStamp)).getTime();
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+      return Math.round(elapsed / 1000) + ' seconds ago';
+    }
+
+    else if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    }
+
+    else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours ago';
+    }
+
+    else if (elapsed < msPerMonth) {
+      return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+    }
+
+    else if (elapsed < msPerYear) {
+      return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+    }
+
+    else {
+      return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+    }
   }
 }
