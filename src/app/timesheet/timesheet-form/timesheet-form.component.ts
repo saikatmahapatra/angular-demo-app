8445052b757c32c4ea8128abc7f67a447bba664e
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-09-18 17:01:13 +0530

    custom class added

--- src/app/timesheet/timesheet-form/timesheet-form.component.ts
+++ src/app/timesheet/timesheet-form/timesheet-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
 @Component({
   selector: 'app-timesheet-form',
   templateUrl: './timesheet-form.component.html',
-  styleUrls: ['./timesheet-form.component.scss']
+  styleUrls: ['./timesheet-form.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TimesheetFormComponent implements OnInit {
   submitted = false;
   loading = false;
   selected!: Date | null;
+  daysSelected: any[] = [];
+  event: any;
+  isSelected = (event: any) => {
+    const date =
+      event.getFullYear() +
+      "-" +
+      ("00" + (event.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("00" + event.getDate()).slice(-2);
+    return this.daysSelected.find(x => x == date) ? "selected" : '';
+  };
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    // Only highligh dates inside the month view.
+    if (view === 'month') {
+      const date = cellDate.getDate();
+
+      // Highlight the 1st and 20th day of each month.
+      return date === 1 || date === 20 ? 'yellow' : '';
+    }
+
+    return '';
+  };
 
   myForm = this.fb.group({
     id: [null],
@@ -31,6 +55,14 @@ export class TimesheetFormComponent implements OnInit {
 
   onSubmit() {
 
+  }
+
+  select(event: any, calendar: any) {
+    const date = event.getFullYear() + "-" + ("00" + (event.getMonth() + 1)).slice(-2) + "-" + ("00" + event.getDate()).slice(-2);
+    const index = this.daysSelected.findIndex(x => x == date);
+    if (index < 0) this.daysSelected.push(date);
+    else this.daysSelected.splice(index, 1);
+    calendar.updateTodaysDate();
   }
 
 }
