c6babcdcd33e7195961c91af2128628fac153d57
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-10-03 20:00:26 +0530

    created

---
+++ src/app/timesheet/view-timesheet-grid/view-timesheet-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-timesheet-grid',
+  templateUrl: './view-timesheet-grid.component.html',
+  styleUrls: ['./view-timesheet-grid.component.scss']
+})
+export class ViewTimesheetGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
