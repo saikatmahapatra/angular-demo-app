4b9b812d73234db59b992a6f5d781ebd52b54b27
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-10-03 20:09:14 +0530

    table data display works

--- src/app/timesheet/view-timesheet-grid/view-timesheet-grid.component.ts
+++ src/app/timesheet/view-timesheet-grid/view-timesheet-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-view-timesheet-grid',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-timesheet-grid.component.scss']
 })
 export class ViewTimesheetGridComponent implements OnInit {
+
+  @Input() dataRow!: any;
 
   constructor() { }
 
