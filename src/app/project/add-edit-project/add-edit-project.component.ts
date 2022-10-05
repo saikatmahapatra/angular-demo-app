6de36ca65de31ef61fb0985bc7436ecd7d7b8703
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-10-05 21:35:13 +0530

    form created

--- src/app/project/add-edit-project/add-edit-project.component.ts
+++ src/app/project/add-edit-project/add-edit-project.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AlertService } from 'src/app/@core/services/alert.service';
+import { ApiService } from 'src/app/@core/services/api.service';
+import { FormValidationService } from 'src/app/@core/services/form-validation.service';
+import { AppConfig } from 'src/app/@utils/const/app.config';
 @Component({
   selector: 'app-add-edit-project',
   templateUrl: './add-edit-project.component.html',
   styleUrls: ['./add-edit-project.component.scss']
 })
 export class AddEditProjectComponent implements OnInit {
+  submitted = false;
+  loading = false;
+  id: any = '';
+  isAdd = true;
+  title = 'Add';
 
-  constructor() { }
+  myForm = this.fb.group({
+    id: [null],
+    action: ['add'],
+    projectNumber: ['', [Validators.required]],
+    projectName: ['', [Validators.required, this.validator.alphaNumericWithSpace]],
+    startDate: ['', [Validators.required]],
+    endDate: ['', [Validators.required]],
+    description: ['']
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private validator: FormValidationService,
+    private apiSvc: ApiService,
+    private alertSvc: AlertService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
   }
 
 }
