2096c9e4e45f0e66240a3e8fa9fd13bc5daabbb7
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-10-08 21:59:55 +0530

    ICellRendererAngularComp added and working

--- src/app/cms/manage-cms/manage-cms.component.ts
+++ src/app/cms/manage-cms/manage-cms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AgGridAngular } from 'ag-grid-angular';
+import { AgGridAngular, ICellRendererAngularComp } from 'ag-grid-angular';
 import { CellClickedEvent, ColDef, GridOptions, GridReadyEvent, ICellRendererParams } from 'ag-grid-community';
 import { ApiService } from 'src/app/@core/services/api.service';
 import { AppConfig } from 'src/app/@utils/const/app.config';
@@ -18,7 +18,7 @@ export class ManageCmsComponent implements OnInit {
     //{ headerName: 'Updated On', field: 'content_updated_on', resizable: true },
     { headerName: 'Created By', field: 'content_created_by', resizable: true, cellRenderer: this.cellRendererCreatedBy },
     { headerName: 'Status', field: 'content_status', resizable: true },
-    { headerName: 'Action', field: '', resizable: false, cellRenderer: this.cellRendererRowAction }
+    { headerName: 'Action', field: '', resizable: false, cellRenderer: cellRendererActionButtons }
   ];
   defaultColDef: ColDef = {
     sortable: true,
@@ -27,7 +27,7 @@ export class ManageCmsComponent implements OnInit {
   totalRecords: any;
 
   constructor(
-    private apiSvc: ApiService
+    public apiSvc: ApiService
   ) { }
 
   ngOnInit(): void {
@@ -45,12 +45,35 @@ export class ManageCmsComponent implements OnInit {
     return params.data?.user_firstname + ' ' + params.data?.user_lastname;
   }
 
-  cellRendererRowAction(params: ICellRendererParams) {
-    let htmlElement = '';
-    htmlElement += '<button class="btn btn-light" type="button"><span class="material-symbols-outlined align-middle">edit</span></button>';
-    htmlElement += '<button class="btn btn-light" type="button"><span class="material-symbols-outlined align-middle">delete</span></button>';
-    return htmlElement;
+}
+
+@Component({
+  selector: 'app-cell-renderer-action-buttons',
+  template: `
+    <button class="btn btn-light" (click)="editRowData()">Edit</button>
+    <button class="btn btn-light" (click)="deleteRowData()">Delete</button>
+  `
+})
+
+export class cellRendererActionButtons extends ManageCmsComponent implements ICellRendererAngularComp {
+  private params!: ICellRendererParams;
+
+  // gets called once before the renderer is used
+  agInit(params: ICellRendererParams): void {
+    this.params = params;
   }
 
+  // gets called whenever the cell refreshes
+  refresh(params: ICellRendererParams): boolean {
+    // set value into cell again
+    return true;
+  }
 
+  editRowData() {
+    console.log(this.params);
+  }
+
+  deleteRowData() {
+    console.log(this.params);
+  }
 }
