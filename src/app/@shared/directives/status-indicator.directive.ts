058bf0ec53b68ad9298223e2a542564a839d3dbe
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-11-12 21:15:06 +0530

    directive onload text show done & working

--- src/app/@shared/directives/status-indicator.directive.ts
+++ src/app/@shared/directives/status-indicator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[stausIndicator]'
@@ -9,9 +9,23 @@ export class StatusIndicatorDirective {
   @Input() value!: string;
   @Input() theme!: string;
 
-  constructor() { 
-    console.log(this.name,  this.value, this.theme);
+  constructor(private el: ElementRef) {}
 
+  @HostListener('window:load') onPageLoad(){
+    this.displayStatus(this.name,  this.value, this.theme);  
+  }
+
+  displayStatus(name: string, val: string, theme: string) {
+    let text = val;
+    let textColor = '';
+    if(name === 'status') {
+      text = val === 'Y' ? 'Active' : 'Inactive';
+    }
+    if(theme === 'color') {
+      textColor = val === 'Y' ? 'green' : 'red';
+    }
+    this.el.nativeElement.innerHTML = text;
+    this.el.nativeElement.style.color = textColor;
   }
 
 }
