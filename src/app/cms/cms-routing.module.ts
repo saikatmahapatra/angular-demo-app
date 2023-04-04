10d568eec1275ff115578b0150f5fd63a4d75402
saikat mahapatra <mahapatra.saikat@gmail.com>
2023-04-04 17:25:34 +0530

    manage holiday component created

--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CmsLayoutComponent } from './cms-layout.component';
 import { ManageCmsComponent } from './manage-cms/manage-cms.component';
 import { AddContentComponent } from './add-content/add-content.component';
+import { ManageHolidaysComponent } from './manage-holidays/manage-holidays.component';
 const routes: Routes = [
   {path: '', component: CmsLayoutComponent, children: [
     {path: '', component: ManageCmsComponent},
     {path: 'add', component: AddContentComponent},
-    {path: 'edit/:id', component: AddContentComponent}
+    {path: 'edit/:id', component: AddContentComponent},
+    {path: 'manage-holidays', component: ManageHolidaysComponent},
+    {path: 'manage-holidays/:id', component: ManageHolidaysComponent}
   ]}
 ];
 
---
+++ src/app/cms/manage-holidays/manage-holidays.component.ts
@@ -0,0 +1,84 @@
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/@core/services/alert.service';
+import { ApiService } from 'src/app/@core/services/api.service';
+import { AppConfig } from 'src/app/@utils/const/app.config';
+
+@Component({
+  selector: 'app-manage-holidays',
+  templateUrl: './manage-holidays.component.html',
+  styleUrls: ['./manage-holidays.component.scss']
+})
+export class ManageHolidaysComponent implements OnInit {
+
+  dataRow: any;
+
+  // Pagination Config
+  currentPageIndex: number = 0;
+  totalRecords: number = 0;
+  itemPerPage: number = 30;
+  itemPerPageDropdown = [10, 20, 30, 50];
+  paginate(event: any) {
+    this.itemPerPage = event.rows;
+    this.currentPageIndex = event.page;
+    this.getContents(this.postType);
+  }
+  // Pagination Config
+
+  postType: any = '';
+  contentCategoryList = [
+    {id: 'notice', name: 'Notice'},
+    {id: 'news', name: 'News'},
+    {id: 'policy', name: 'HR Policy'},
+    {id: 'mandatory_holiday', name: 'Mandatory Holiday'},
+    {id: 'optional_holiday', name: 'Optional Holiday'},
+  ];
+
+  constructor(
+    public apiSvc: ApiService,
+    private router: Router,
+    private alertSvc: AlertService
+  ) { }
+
+  ngOnInit(): void {
+    this.getContents(this.postType);
+  }
+
+  getContents(type?: string) {
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+    if(type) {
+      params = params.append('type', type)
+    }
+    headers = headers.set('perPage', String(this.itemPerPage));
+    headers = headers.set('page', String(this.currentPageIndex));
+    this.apiSvc.get(AppConfig.apiUrl.getPosts, { headers: headers, params: params }).subscribe((response: any) => {
+      this.totalRecords = response?.data['num_rows'];
+      this.dataRow = response?.data['data_rows'];
+    });
+  }
+
+  editPost(data: any) {
+    this.router.navigate(['/cms/edit', data.id]);
+  }
+
+  deletePost(data: any) {
+    let queryParams = new HttpParams();
+    if (data.id) {
+      queryParams = queryParams.append('id', data.id);
+    }
+    let options = {};
+    options = { params: queryParams };
+    this.apiSvc.delete(AppConfig.apiUrl.deletePost, options).subscribe((response: any) => {
+      this.alertSvc.success(response.message);
+      this.getContents(this.postType);
+    });
+  }
+
+  postTypeChange() {
+    this.currentPageIndex = 0;
+    this.getContents(this.postType);
+  }
+
+}
