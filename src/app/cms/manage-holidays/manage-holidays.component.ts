31ec6ae2fb60fa1ae818180b2addb1e8256cc9c5
saikat mahapatra <mahapatra.saikat@gmail.com>
2023-04-05 11:59:08 +0530

    holiday management added

--- src/app/cms/manage-holidays/manage-holidays.component.ts
+++ src/app/cms/manage-holidays/manage-holidays.component.ts
@@ -1,7 +1,9 @@
 import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/@core/services/alert.service';
 import { ApiService } from 'src/app/@core/services/api.service';
+import { FormValidationService } from 'src/app/@core/services/form-validation.service';
 import { AppConfig } from 'src/app/@utils/const/app.config';
 
 @Component({
@@ -27,15 +29,32 @@ export class ManageHolidaysComponent implements OnInit {
     this.getHolidays();
   }
   // Pagination Config
+
+
+  myForm = this.fb.group({
+    id: [null],
+    action: ['add'],
+    date: ['', [Validators.required]],
+    occasion: ['', [Validators.required]],
+    type: ['', [Validators.required]]
+  });
+  submitted = false;
+  loading = false;
+  mode = 'add';
+  formTitle = "Add Holiday";
+  buttonText = "Submit";
+
   constructor(
     public apiSvc: ApiService,
-    private alertSvc: AlertService
+    private alertSvc: AlertService,
+    private fb: UntypedFormBuilder,
+    private validator: FormValidationService
   ) {
 
-   }
+  }
 
   ngOnInit(): void {
-    for(let y = this.startYear; y <= this.endYear; y++) {
+    for (let y = this.startYear; y <= this.endYear; y++) {
       this.yearList.push(y);
     }
     this.getHolidays();
@@ -44,7 +63,7 @@ export class ManageHolidaysComponent implements OnInit {
   getHolidays() {
     let headers = new HttpHeaders();
     let params = new HttpParams();
-    if(this.selectedYear) {
+    if (this.selectedYear) {
       params = params.append('year', this.selectedYear)
     }
     headers = headers.set('perPage', String(this.itemPerPage));
@@ -57,6 +76,86 @@ export class ManageHolidaysComponent implements OnInit {
 
   yearChange() {
     this.getHolidays();
+  }
+
+  editItem(data: any) {
+    if (data.id) {
+      this.mode = 'update';
+      this.formTitle = "Edit Holiday";
+      this.buttonText = "Update";
+      this.myForm.patchValue({
+        id: data?.id,
+        action: 'edit',
+        date: new Date(data?.holiday_date),
+        occasion: data?.holiday_description,
+        type: data?.holiday_type
+      });
+    }
+  }
+
+  deleteItem(data: any) {
+    let queryParams = new HttpParams();
+    if (data.id) {
+      queryParams = queryParams.append('id', data.id);
+    }
+    let options = {};
+    options = { params: queryParams };
+    this.apiSvc.delete(AppConfig.apiUrl.deleteHoliday, options).subscribe({
+      next: (response: any) => {
+        this.setAddMode();
+        this.alertSvc.success(response.message);
+        this.getHolidays();
+      }
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+    if (this.myForm.valid && this.myForm.get('action')?.value === 'add') {
+      this.apiSvc.post(AppConfig.apiUrl.addHoliday, this.myForm.value).subscribe({
+        next: (response: any) => {
+          this.alertSvc.success(response.message, true);
+          this.getHolidays();
+          this.setAddMode();
+        },
+        error: () => { this.loading = false; },
+        complete: () => { this.loading = false; }
+      });
+    }
+    else if (this.myForm.valid && this.myForm.get('action')?.value === 'edit' && this.myForm.get('id')?.value) {
+      this.apiSvc.put(AppConfig.apiUrl.updateHoliday, this.myForm.value).subscribe({
+        next: (response: any) => {
+          this.alertSvc.success(response.message, true);
+          this.getHolidays();
+          this.setAddMode();
+        },
+        error: () => { this.loading = false; },
+        complete: () => { this.loading = false; }
+      });
+    }
+    else {
+      this.loading = false;
+      this.validator.validateAllFormFields(this.myForm);
+    }
+
+  }
+
+  setAddMode() {
+    this.formTitle = "Add Holiday";
+    this.buttonText = "Submit";
+    this.mode = 'add';
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.myForm.reset({
+      id: null,
+      action: 'add',
+      date: '',
+      occasion: '',
+      type: ''
+    });
   }
 
 }
