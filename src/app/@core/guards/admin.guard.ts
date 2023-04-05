030765e993cd1ee6cd2725cc0b9893492fd1673b
saikat mahapatra <mahapatra.saikat@gmail.com>
2023-04-05 15:09:08 +0530

    admin guard added

---
+++ src/app/@core/guards/admin.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, CanActivateChild } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { AlertService } from 'src/app/@core/services/alert.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService, private alertService: AlertService) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const userRole = this.authService.getRoleId();
+    if (userRole == 1) {
+      return true;
+    } else {
+      this.authService.logout();
+      this.router.navigate(['unauthorized']);
+      return false;
+    }
+  }
+}
--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -6,14 +6,15 @@ import { ManageCmsComponent } from './manage-cms/manage-cms.component';
 import { AddContentComponent } from './add-content/add-content.component';
 import { ViewHolidaysComponent } from './view-holidays/view-holidays.component';
 import { ManageHolidaysComponent } from './manage-holidays/manage-holidays.component';
+import { AdminGuard } from '../@core/guards/admin.guard';
 const routes: Routes = [
   {
     path: '', component: CmsLayoutComponent, children: [
-      { path: '', component: ManageCmsComponent },
-      { path: 'add', component: AddContentComponent },
-      { path: 'edit/:id', component: AddContentComponent },
+      { path: '', canActivate: [AdminGuard], component: ManageCmsComponent },
+      { path: 'add', canActivate: [AdminGuard], component: AddContentComponent },
+      { path: 'edit/:id', canActivate: [AdminGuard], component: AddContentComponent },
       { path: 'holiday-calendar', component: ViewHolidaysComponent },
-      { path: 'holiday-management', component: ManageHolidaysComponent }
+      { path: 'holiday-management', canActivate: [AdminGuard], component: ManageHolidaysComponent }
     ]
   }
 ];
