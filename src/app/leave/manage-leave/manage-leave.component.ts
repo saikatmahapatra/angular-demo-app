0b172730876146e0b6f4e30420c6c40e2cf7730b
saikat mahapatra <mahapatra.saikat@gmail.com>
2023-04-06 19:15:22 +0530

    leave

--- src/app/leave/manage-leave/manage-leave.component.ts
+++ src/app/leave/manage-leave/manage-leave.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-leave.component.scss']
 })
 export class ManageLeaveComponent implements OnInit {
-
+  dataRow = [];
+  // Pagination Config
+  currentPageIndex: number = 0;
+  totalRecords: number = 0;
+  itemPerPage: number = 30;
+  itemPerPageDropdown = [10, 20, 30, 50];
+  paginate(event: any) {
+    this.itemPerPage = event.rows;
+    this.currentPageIndex = event.page;
+  }
+  // Pagination Config
   constructor() { }
 
   ngOnInit(): void {
