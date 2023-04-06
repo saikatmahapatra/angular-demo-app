f494e88e41c75460a8b7544d3b360c1eda7586dd
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-04-06 22:47:28 +0530

    need to check why validation is not working

--- src/app/leave/manage-leave/manage-leave.component.ts
+++ src/app/leave/manage-leave/manage-leave.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/@core/services/alert.service';
+import { FormValidationService } from 'src/app/@core/services/form-validation.service';
 
 @Component({
   selector: 'app-manage-leave',
@@ -24,8 +25,8 @@ export class ManageLeaveComponent implements OnInit {
   // Pagination Config
 
   searchForm = this.fb.group({
-    fromDate: [new Date(), Validators.required],
-    toDate: [new Date(), Validators.required],
+    fromDate: [null, Validators.required],
+    toDate: [null, Validators.required],
     leaveStatus: ['', Validators.required]
   });
 
@@ -37,14 +38,22 @@ export class ManageLeaveComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private alertSvc: AlertService,
-    private router: Router
+    private router: Router,
+    private validator: FormValidationService
   ) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    
+    this.submitted = true;
+    this.loading = true;
+    if (this.searchForm.valid) {
+      
+    } else {
+      this.loading = false;
+      this.validator.validateAllFormFields(this.searchForm);
+    }
   }
 
 }
