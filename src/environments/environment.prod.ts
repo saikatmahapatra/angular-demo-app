622b0d1ec062addbbec98df4b5a8396a9fe26068
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-04-13 22:18:03 +0530

    modification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   name: 'prod',
   production: true,
-  title: "MyApp::UEIPL",
-  productName: "UEIPL MyApp",
+  title: "MyApp - United Exploration India",
+  productName: "MyApp",
   apiBaseUrl: "http://portalapi.ueipl.co.in/api/",
   mockAPIUrl: "http://localhost:7878/",
   useMockServer: false,
-  copyrightInfo: "Copyright &copy; 2023 United Exploration",
+  copyrightInfo: "Copyright &copy; 2023 United Exploration India",
   version: "v1.0"
 };
