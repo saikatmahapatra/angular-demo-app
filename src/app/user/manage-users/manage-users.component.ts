068fa53f83c2ae5f7770118dd6416b5d32706293
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-04-19 23:04:40 +0530

    reset pagination on search

--- src/app/user/manage-users/manage-users.component.ts
+++ src/app/user/manage-users/manage-users.component.ts
@@ -29,8 +29,10 @@ export class ManageUsersComponent implements OnInit {
   totalRecords: number = 0;
   itemPerPage: number = 10;
   itemPerPageDropdown = [10, 20, 30, 50];
+  first: number = 0;
   searchKeyword: any = '';
   paginate(event: any) {
+    console.log(event);
     this.itemPerPage = event.rows;
     this.currentPageIndex = event.page;
     this.getUsersList();
@@ -82,7 +84,15 @@ export class ManageUsersComponent implements OnInit {
 
   getSearchKeyword(str: string) {
     this.searchKeyword = str;
+    this.resetPagination();
     this.getUsersList();
+  }
+
+  resetPagination() {
+    this.currentPageIndex = 0;
+    this.totalRecords = 0;
+    this.itemPerPage = 10;
+    this.first = 0;
   }
 
 }
