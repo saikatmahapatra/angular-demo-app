ef7b5af36b0076ec4f4cdfb85c78ff6199e74d18
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-04-23 17:45:31 +0530

    icon related changes

--- src/app/@shared/components/icon/icon.component.ts
+++ src/app/@shared/components/icon/icon.component.ts
@@ -11,7 +11,7 @@ export class IconComponent implements OnInit {
   @Input() width: number = 16;
   @Input() height: number = 16;
   @Input() fill: string = 'currentColor';
-  @Input() styleCSS: any = '';
+  @Input() styleClass: any = '';
   @Input() svg: boolean = false;
 
   // Icons
@@ -19,25 +19,45 @@ export class IconComponent implements OnInit {
     'home': { primeng: 'pi pi-home', bootstrap: '' },
     'user': { primeng: 'pi pi-user', bootstrap: '' },
     'users': { primeng: 'pi pi-users', bootstrap: '' },
-    'plus': { primeng: 'pi pi-plus', bootstrap: '' },
+    'add': { primeng: 'pi pi-plus', bootstrap: '' },
     'edit': { primeng: 'pi pi-pencil', bootstrap: '' },
     'userEdit': { primeng: 'pi pi-user-edit', bootstrap: '' },
     'delete': { primeng: 'pi pi-trash', bootstrap: '' },
-    'info': { primeng: 'pi pi-info', bootstrap: '' },
-    'infoCircle': { primeng: 'pi pi-info-circle', bootstrap: '' },
+    'info': { primeng: 'pi pi-info-circle', bootstrap: '' },
+    'success': { primeng: 'pi pi-check-circle', bootstrap: '' },
+    'error': { primeng: 'pi pi-exclamation-circle', bootstrap: '' },
+    'warning': { primeng: 'pi pi-exclamation-triangle', bootstrap: '' },
     'admin': { primeng: 'pi pi-lock', bootstrap: '' },
     'selfService': { primeng: 'pi pi-folder', bootstrap: '' },
     'angleLeft': { primeng: 'pi pi-angle-left', bootstrap: '' },
-    'currency': { primeng: 'pi pi-dollart', bootstrap: '' },
+    'angleUp': { primeng: 'pi pi-angle-up', bootstrap: '' },
+    'angleRight': { primeng: 'pi pi-angle-right', bootstrap: '' },
+    'arrowRight': { primeng: 'pi pi-arrow-right', bootstrap: '' },
+    'currency': { primeng: 'pi pi-dollar', bootstrap: '' },
     'clock': { primeng: 'pi pi-clock', bootstrap: '' },
     'globe': { primeng: 'pi pi-globe', bootstrap: '' },
-    'location': { primeng: 'pi pi-map-marker', bootstrap: '' },
+    'map': { primeng: 'pi pi-map-marker', bootstrap: '' },
     'phone': { primeng: 'pi pi-phone', bootstrap: '' },
     'email': { primeng: 'pi pi-envelope', bootstrap: '' },
     'sun': { primeng: 'pi pi-sun', bootstrap: '' },
     'moon': { primeng: 'pi pi-moon', bootstrap: '' },
-    'status': { primeng: 'pi pi-circle-fill', bootstrap: '' },
-    
+    'circle': { primeng: 'pi pi-circle-fill', bootstrap: '' },
+    'signout': { primeng: 'pi pi-sign-out', bootstrap: '' },
+    'notification': { primeng: 'pi pi-bell', bootstrap: '' },
+    'linechart': { primeng: 'pi pi-chart-line', bootstrap: '' },
+    'book': { primeng: 'pi pi-book', bootstrap: '' },
+    'job': { primeng: 'pi pi-briefcase', bootstrap: '' },
+    'heart': { primeng: 'pi pi-heart', bootstrap: '' },
+    'actionRequired': { primeng: 'pi pi-question-circle', bootstrap: '' },
+    'check': { primeng: 'pi pi-check-circle', bootstrap: '' },
+    'cross': { primeng: 'pi pi-times-circle', bootstrap: '' },
+    'question': { primeng: 'pi pi-question-circle', bootstrap: '' },
+    'send': { primeng: 'pi pi-send', bootstrap: '' },
+    'timesheet': { primeng: 'pi pi-calendar', bootstrap: '' },
+    'projects': { primeng: 'pi pi-th-large', bootstrap: '' },
+    'search': { primeng: 'pi pi-search', bootstrap: '' },
+    'settings': { primeng: 'pi pi-cog', bootstrap: '' },
+    'upload': { primeng: 'pi pi-upload', bootstrap: '' }
   };
 
   constructor() {
