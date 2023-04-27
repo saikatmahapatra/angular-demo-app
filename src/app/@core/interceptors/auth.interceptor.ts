14dafc98af69e228a6140253f233fce1ff05301c
saikat mahapatra <mahapatra.saikat@gmail.com>
2023-04-27 16:03:42 +0530

    clear server side api error before next call

--- src/app/@core/interceptors/auth.interceptor.ts
+++ src/app/@core/interceptors/auth.interceptor.ts
@@ -9,17 +9,19 @@ import { finalize, Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { LoaderService } from '../services/loader.service';
 import { AppConfig } from 'src/app/@utils/const/app.config';
+import { AlertService } from '../services/alert.service';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authSvc: AuthService, private loader: LoaderService) {}
+  constructor(private authSvc: AuthService, private loader: LoaderService, private alertSvc: AlertService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authToken = this.authSvc.getToken();
     const isMockServer = AppConfig.useMockServer;
     this.loader.show();
+    this.alertSvc.clear();
     if(authToken && !isMockServer) {
       const clonedReq = request.clone({
         setHeaders: {
