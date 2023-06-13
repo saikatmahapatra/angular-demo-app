a6133a0c87ee1f11fe72615502a40b286273687a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-06-13 15:47:46 +0530

    enum added

---
+++ src/app/@utils/enums/enums.ts
@@ -0,0 +1,11 @@
+export enum EnumStatus {
+    Y = 'Active',
+    N = 'Inactive',
+    A = 'Archived',
+}
+
+export enum EnumLeveType {
+    PL = 'PL-Privileged Leave',
+    CL = 'CL-Casual Leave',
+    SL = 'SL-Sick Leave',
+}
