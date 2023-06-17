05139b7d7789fd94350aef70c39b0df56ea4d477
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-06-17 15:52:30 +0530

    inline form created

--- src/app/user/data-chart/data-chart.component.ts
+++ src/app/user/data-chart/data-chart.component.ts
@@ -1,5 +1,6 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { AlertService } from 'src/app/@core/services/alert.service';
 import { ApiService } from 'src/app/@core/services/api.service';
@@ -12,12 +13,45 @@ import { addressType, userStatus } from 'src/app/@utils/const/data.array';
   styleUrls: ['./data-chart.component.scss']
 })
 export class DataChartComponent implements OnInit {
+  submitted = false;
+  loading = false;
   routedFromPageIndex = 0;
-  constructor(private apiSvc: ApiService, private alertSvc: AlertService,
+  
+  myForm = this.fb.group({
+    action: ['searchData'],
+    fromToDate: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private apiSvc: ApiService, 
+    private alertSvc: AlertService,
     private activatedRoute: ActivatedRoute,
-    private router: Router) { }
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.routedFromPageIndex = history.state['manageUserPageIndex'] || 0;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+    console.log(this.myForm.value);
+    // if (this.approvers.length > 0 && this.myForm.valid && this.myForm.get('action')?.value === 'applyLeave') {
+    //   this.apiSvc.post(AppConfig.apiUrl.applyLeave, this.myForm.value).subscribe({
+    //     next: (response: any) => {
+    //       this.alertSvc.success(response.message, true);
+    //       this.resetFormValue();
+    //       this.router.navigate(['/leave/history']);
+    //     },
+    //     error: () => { this.loading = false; },
+    //     complete: () => { this.loading = false; }
+    //   });
+    // }
+    // else {
+    //   this.loading = false;
+    //   this.validator.validateAllFormFields(this.myForm);
+    // }
   }
 }
