4ecab03a17a87dd28fb961b200613274075fb0d8
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2023-06-26 11:49:29 +0530

    SRBAC added

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SettingsLayoutComponent } from './settings-layout.component';
 import { SiteSettingsComponent } from './site-settings/site-settings.component';
 import { SiteMetaComponent } from './site-meta/site-meta.component';
+import { SrbacComponent } from './srbac/srbac.component';
 
 const routes: Routes = [
   {
     path: '', component: SettingsLayoutComponent,
     children: [
       { path: '', component: SiteSettingsComponent },
-      { path: 'site-meta', component: SiteMetaComponent }
+      { path: 'site-meta', component: SiteMetaComponent },
+      { path: 'srbac', component: SrbacComponent }
     ]
   },
 
