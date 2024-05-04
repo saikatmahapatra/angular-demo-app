2346727c629f8da87cbd00182dbd2b5fb97f347f
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2024-05-04 10:24:12 +0530

    primeng alert implemented

--- src/app/@shared/components/alert-message/alert-message.component.ts
+++ src/app/@shared/components/alert-message/alert-message.component.ts
@@ -1,53 +1,33 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AlertService } from '../../../@core/services/alert.service';
+import { Message, MessageService } from 'primeng/api';
 @Component({
   selector: 'app-alert-message',
   templateUrl: './alert-message.component.html',
-  styleUrls: ['./alert-message.component.scss']
+  styleUrls: ['./alert-message.component.scss'],
+  providers: [MessageService]
 })
 export class AlertMessageComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription = new Subscription;
-  message: any;
-
-  constructor(private alertService: AlertService) { }
+  messages!: Message[];
+  constructor(private alertService: AlertService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.subscription = this.alertService.getAlert()
       .subscribe(message => {
-        //window.scrollTo(0, 0);
-        switch (message && message.type) {
-          case 'success':
-            message.cssClass = 'alert-success';
-            message.icon = 'success';
-            break;
-          case 'error':
-            message.cssClass = 'alert-danger';
-            message.icon = 'error';
-            break;
-          case 'info':
-            message.cssClass = 'alert-info';
-            message.icon = 'info';
-            break;
-          case 'warning':
-            message.cssClass = 'alert-warning';
-            message.icon = 'warning';
-            break;
-        }
-
-        this.message = message;
-        // setTimeout(() => {
-        //   this.dismissAlert()
-        // }, 8000)
+        window.scrollTo(0, 0);
+        this.messages = [{ severity: message.type, summary: this.capitalizeFLetter(String(message.type)), detail: message.text }];
+        //this.messageService.add({ severity: message.type, summary: message.type, detail: message.text });
       });
+  }
+
+  capitalizeFLetter(string: string) {
+    return (string.charAt(0).toUpperCase() + string.slice(1));
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }
-
-  dismissAlert() {
-    this.message = {};
   }
 }
