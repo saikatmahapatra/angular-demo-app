8118c5e19be23141151c70bc755c50d8b2505678
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2024-05-20 13:57:12 +0530

    example added

--- src/app/ng-example/prime-ng-ui-kit/prime-ng-ui-kit.component.ts
+++ src/app/ng-example/prime-ng-ui-kit/prime-ng-ui-kit.component.ts
@@ -186,6 +186,8 @@ export class PrimeNgUiKitComponent implements OnInit {
   csvPayload: any = [];
   // file uploader related ends here
 
+  serverSideMsg: Message[] = [];
+
   constructor(private messageService: MessageService) { }
 
   ngOnInit() {
@@ -203,6 +205,28 @@ export class PrimeNgUiKitComponent implements OnInit {
       label: 'Confirmation'
     }
     ];
+    this.serverSideMsg = [
+      { severity: 'success', summary: 'Success', detail: 'Message content' },
+      { severity: 'info', summary: 'Info', detail: 'Message content' },
+      { severity: 'warn', summary: 'Warning', detail: 'Message content' },
+      { severity: 'error', summary: 'Error', detail: 'Message content' }
+    ];
+  }
+
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+  }
+
+  showInfo() {
+    this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content' });
+  }
+
+  showWarn() {
+    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: 'Message Content' });
+  }
+
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Message Content' });
   }
 
   openUploadModal() {
