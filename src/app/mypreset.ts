815bda2a7293134084804c9ad38b17db66e1e3d7
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-06-10 23:31:59 +0530

    theme customization

--- src/app/mypreset.ts
+++ src/app/mypreset.ts
@@ -51,6 +51,37 @@ const MyPreset = definePreset(Aura, {
         },
       },
     },
+    focusRing: {
+      width: '2px',
+      style: 'dashed',
+      color: '{primary.color}',
+      offset: '1px',
+    },
+    components: {
+      card: {
+        colorScheme: {
+          light: {
+            root: {
+              background: '{surface.0}',
+              color: '{surface.700}',
+            },
+            subtitle: {
+              color: '{surface.500}',
+            },
+          },
+          dark: {
+            root: {
+              background: '{surface.900}',
+              color: '{surface.0}',
+            },
+            subtitle: {
+              color: '{surface.400}',
+            },
+          },
+        },
+      },
+    },
+    
   },
 });
 
